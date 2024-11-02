7df9906991a857d43ef55fc94181052e231c8e6e
vimuths123 <vimuths@yahoo.com>
2024-11-02 20:56:24 +0530

    Test with google

--- src/app/fonts.js
+++ src/app/fonts.js
@@ -1,6 +1,7 @@
 // import { Merriweather, Lexend, Merriweather_Sans, Poppins, Manrope, Varta, DM_Serif_Display, Jost, Roboto } from 'next/font/google';
+import { Merriweather } from 'next/font/google';
 
-// export const merriweather = Merriweather({ weight: ['300', '400', '700', '900'], style: ['normal', 'italic'], subsets: ['latin'] });
+export const merriweather = Merriweather({ weight: ['300', '400', '700', '900'], style: ['normal', 'italic'], subsets: ['latin'] });
 // export const lexend = Lexend({ weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'], subsets: ['latin'] });
 // export const merriweatherSans = Merriweather_Sans({ weight: ['300', '400', '500', '600', '700', '800'], subsets: ['latin'] });
 // export const poppins = Poppins({ weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'], subsets: ['latin'] });
@@ -12,50 +13,98 @@
 
 
 
-import localFont from 'next/font/local';
+// import localFont from 'next/font/local';
 
-export const merriweather = localFont({
-  src: [
-    {
-      path: './fonts/merriweather/Merriweather-Light.ttf',
-      weight: '300',
-      style: 'normal',
-    },
-    {
-      path: './fonts/merriweather/Merriweather-LightItalic.ttf',
-      weight: '300',
-      style: 'italic',
-    },
-    {
-      path: './fonts/merriweather/Merriweather-Regular.ttf',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: './fonts/merriweather/Merriweather-Italic.ttf',
-      weight: '400',
-      style: 'italic',
-    },
-    {
-      path: './fonts/merriweather/Merriweather-Bold.ttf',
-      weight: '700',
-      style: 'normal',
-    },
-    {
-      path: './fonts/merriweather/Merriweather-BoldItalic.ttf',
-      weight: '700',
-      style: 'italic',
-    },
-    {
-      path: './fonts/merriweather/Merriweather-Black.ttf',
-      weight: '900',
-      style: 'normal',
-    },
-    {
-      path: './fonts/merriweather/Merriweather-BlackItalic.ttf',
-      weight: '900',
-      style: 'italic',
-    },
-  ],
-  display: 'swap', // Use "swap" to display fallback text until the font loads
-});
+// export const merriweather = localFont({
+//   src: [
+//     {
+//       path: '../../public/fonts/merriweather/Merriweather-Light.ttf',
+//       weight: '300',
+//       style: 'normal',
+//     },
+//     {
+//       path: '../../public/fonts/merriweather/Merriweather-LightItalic.ttf',
+//       weight: '300',
+//       style: 'italic',
+//     },
+//     {
+//       path: '../../public/fonts/merriweather/Merriweather-Regular.ttf',
+//       weight: '400',
+//       style: 'normal',
+//     },
+//     {
+//       path: '../../public/fonts/merriweather/Merriweather-Italic.ttf',
+//       weight: '400',
+//       style: 'italic',
+//     },
+//     {
+//       path: '../../public/fonts/merriweather/Merriweather-Bold.ttf',
+//       weight: '700',
+//       style: 'normal',
+//     },
+//     {
+//       path: '../../public/fonts/merriweather/Merriweather-BoldItalic.ttf',
+//       weight: '700',
+//       style: 'italic',
+//     },
+//     {
+//       path: '../../public/fonts/merriweather/Merriweather-Black.ttf',
+//       weight: '900',
+//       style: 'normal',
+//     },
+//     {
+//       path: '../../public/fonts/merriweather/Merriweather-BlackItalic.ttf',
+//       weight: '900',
+//       style: 'italic',
+//     },
+//   ],
+//   display: 'swap', // Use "swap" to display fallback text until the font loads
+// });
+
+// import localFont from 'next/font/local';
+
+// export const merriweather = localFont({
+//   src: [
+//     {
+//       path: './fonts/merriweather/Merriweather-Light.ttf',
+//       weight: '300',
+//       style: 'normal',
+//     },
+//     {
+//       path: './fonts/merriweather/Merriweather-LightItalic.ttf',
+//       weight: '300',
+//       style: 'italic',
+//     },
+//     {
+//       path: './fonts/merriweather/Merriweather-Regular.ttf',
+//       weight: '400',
+//       style: 'normal',
+//     },
+//     {
+//       path: './fonts/merriweather/Merriweather-Italic.ttf',
+//       weight: '400',
+//       style: 'italic',
+//     },
+//     {
+//       path: './fonts/merriweather/Merriweather-Bold.ttf',
+//       weight: '700',
+//       style: 'normal',
+//     },
+//     {
+//       path: './fonts/merriweather/Merriweather-BoldItalic.ttf',
+//       weight: '700',
+//       style: 'italic',
+//     },
+//     {
+//       path: './fonts/merriweather/Merriweather-Black.ttf',
+//       weight: '900',
+//       style: 'normal',
+//     },
+//     {
+//       path: './fonts/merriweather/Merriweather-BlackItalic.ttf',
+//       weight: '900',
+//       style: 'italic',
+//     },
+//   ],
+//   display: 'swap', // Use "swap" to display fallback text until the font loads
+// });
